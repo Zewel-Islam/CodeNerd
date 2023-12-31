87bc74e7102b0ac0f3086f0b45f8acbf9d528c10
Zewel-Islam <zewelislam37@gmail.com>
2023-12-31 16:02:08 +0600

    Remove all files

--- Temperature Converter/dist/script.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function fahrenheitToCelsius() {
-  return (fahrenheit - 32) / 1.8;
-}
-
-var celsiusInputElement = document.getElementById(celsiusInput);
-var inputValue = celsiusInputElement.value;
