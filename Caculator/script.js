6fe318e9120cd949dbe5696bdc221442fadb0907
Zewel-Islam <zewelislam37@gmail.com>
2023-12-31 15:23:11 +0600

    Add calculator

--- Caculator/script.js
+++ Caculator/script.js
@@ -1,12 +1,14 @@
-// document.getElementById("convertButton").addEventListener("click", function () {
-//   let celsiusInputValue = document.getElementById("celsiusInput").value;
-//   let fahrenheitResult = (celsiusInputValue * 9) / 5 + 32;
-//   document.getElementById("result").innerText = fahrenheitResult.toFixed(2);
-// });
-
-function temperatureResult() {
-  let celsiusInputValue = document.getElementById("celsiusInput").value;
-  let convertButton = document.getElementById("convertButton");
-  let fahrenheitResult = (celsiusInputValue * 9) / 5 + 32;
-  document.getElementById("result").innerText = fahrenheitResult.toFixed(2);
+function addTwo() {
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  let sum = num1 + num2;
+  document.getElementById("result").innerText = sum;
 }
+function minusTwo() {
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  let sum = num1 - num2;
+  document.getElementById("result").innerText = sum;
+}
+
+
